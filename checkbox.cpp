05ca1c816035da4a96b4a0cd8b838ed0efeb4562
bajaribalazs <50806619+bajaribalazs@users.noreply.github.com>
2019-05-21 01:20:41 +0200

    2 jatekos (proba2.2)

--- checkbox.cpp
+++ checkbox.cpp
@@ -8,7 +8,8 @@ checkbox::checkbox(int x, int y, int sx, int sy)
     focus=false;
 }
 
-void checkbox::unselect(){
+void checkbox::unselect()
+{
     _focused = false;
 }
 
@@ -16,52 +17,79 @@ void checkbox::draw()
 {
     gout << move_to(_x, _y) << color(255,255,255) << box(_size_x, _size_y);
     gout << move_to(_x+2, _y+2) << color(0,0,0) << box(_size_x-4, _size_y-4);
-    if (counter % 2 == 1 && focus) {
-        gout << color(255,255,255);
+    if(focus)
+    {
+        gout << color(255,0,0);
         gout << move_to(_x+4, _y+4) << line(_size_x-8, _size_y-8);
         gout << move_to(_x+5, _y+4) << line(_size_x-8, _size_y-8);
         gout << move_to(_x+_size_x-4, _y+4) << line(-_size_x+8, _size_y-8);
         gout << move_to(_x+_size_x-5, _y+4) << line(-_size_x+8, _size_y-8);
-    } if(counter % 2 == 0 && focus){
-        gout << color(0,255,255);
-        gout << move_to(_x+4, _y+4) << line(_size_x-8, _size_y-8);
-        gout << move_to(_x+5, _y+4) << line(_size_x-8, _size_y-8);
-        gout << move_to(_x+_size_x-4, _y+4) << line(-_size_x+8, _size_y-8);
-        gout << move_to(_x+_size_x-5, _y+4) << line(-_size_x+8, _size_y-8);
+
     }
+
+
 }
 
 void checkbox::handle(event ev)
 {
-    if (ev.type == ev_key && (ev.keycode == key_enter || ev.keycode == ' ')) {
-            focus = !focus;
+    if (ev.type == ev_key && (ev.keycode == key_enter || ev.keycode == ' '))
+    {
+        focus = !focus;
     }
     if (is_selected(ev.pos_x, ev.pos_y) && ev.type == ev_mouse && ev.button==btn_left)
-        {
-            focus = !focus;
-            if (counter % 2){
-                statusz = 1;
-
-
-            } else {
-                statusz = 2;
-            }
-            counter = counter +1;
-
-        }
+    {
+        focus = !focus;
+        statusz=1;
+    }
 
 
 }
 
-bool checkbox::is_focused(){
+void checkbox::player_1()
+{
+    if (!focus&&used==false)
+    {
+        gout << color(0,0,255);
+        gout << move_to(_x+4, _y+4) << line(_size_x-8, _size_y-8);
+        gout << move_to(_x+5, _y+4) << line(_size_x-8, _size_y-8);
+        gout << move_to(_x+_size_x-4, _y+4) << line(-_size_x+8, _size_y-8);
+        gout << move_to(_x+_size_x-5, _y+4) << line(-_size_x+8, _size_y-8);
+        used=true;
+        p=0;
+    }
+}
+void checkbox::player_2()
+{
+    if (!focus&&used==false)
+    {
+        gout << color(255,0,0);
+        gout << move_to(_x+4, _y+4) << line(_size_x-8, _size_y-8);
+        gout << move_to(_x+5, _y+4) << line(_size_x-8, _size_y-8);
+        gout << move_to(_x+_size_x-4, _y+4) << line(-_size_x+8, _size_y-8);
+        gout << move_to(_x+_size_x-5, _y+4) << line(-_size_x+8, _size_y-8);
+        used=true;
+        p=1;
+    }
+}
+
+bool checkbox::is_used()
+{
+    return used;
+}
+
+
+bool checkbox::is_focused()
+{
     return _focused;
 }
 
-string checkbox::get_value(){
+string checkbox::get_value()
+{
 
 }
 
-int checkbox::get_statusz(){
+int checkbox::get_statusz()
+{
     return statusz;
 
 }
