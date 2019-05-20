42f9a023984aa292430b267afa7416a419a2137f
bajaribalazs <50806619+bajaribalazs@users.noreply.github.com>
2019-05-20 03:44:52 +0200

    2 jatekos

--- checkbox.cpp
+++ checkbox.cpp
@@ -16,8 +16,14 @@ void checkbox::draw()
 {
     gout << move_to(_x, _y) << color(255,255,255) << box(_size_x, _size_y);
     gout << move_to(_x+2, _y+2) << color(0,0,0) << box(_size_x-4, _size_y-4);
-    if (focus) {
+    if (counter % 2 == 1 && focus) {
         gout << color(255,255,255);
+        gout << move_to(_x+4, _y+4) << line(_size_x-8, _size_y-8);
+        gout << move_to(_x+5, _y+4) << line(_size_x-8, _size_y-8);
+        gout << move_to(_x+_size_x-4, _y+4) << line(-_size_x+8, _size_y-8);
+        gout << move_to(_x+_size_x-5, _y+4) << line(-_size_x+8, _size_y-8);
+    } if(counter % 2 == 0 && focus){
+        gout << color(0,255,255);
         gout << move_to(_x+4, _y+4) << line(_size_x-8, _size_y-8);
         gout << move_to(_x+5, _y+4) << line(_size_x-8, _size_y-8);
         gout << move_to(_x+_size_x-4, _y+4) << line(-_size_x+8, _size_y-8);
@@ -35,6 +41,8 @@ void checkbox::handle(event ev)
             focus = !focus;
             if (counter % 2){
                 statusz = 1;
+
+
             } else {
                 statusz = 2;
             }
